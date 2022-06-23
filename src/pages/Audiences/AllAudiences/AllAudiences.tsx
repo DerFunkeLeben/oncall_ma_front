6421f9a7117ec9d9be894cfba64946366ef99dd9
Андрей Карпенко <a.karpenko@onpoint.ru>
2022-06-23 22:51:25 +0300

    опять переделываю таблицу

--- src/pages/Audiences/AllAudiences/AllAudiences.tsx
+++ src/pages/Audiences/AllAudiences/AllAudiences.tsx
@@ -1,21 +1,23 @@
-import { FC, useEffect, useState, useRef } from 'react'
+import { FC, useState, useRef } from 'react'
 import { useHistory, useRouteMatch } from 'react-router-dom'
 import cx from 'classnames'
 
 import PageHead from 'components/PageHead/PageHead'
 import Button from 'components/parts/Button/Button'
 import DropDown from 'components/parts/DropDown/DropDown'
-import ScrollTable from 'components/Table/ScrollTable'
+import ScrollTable from 'components/TableDiv/ScrollTable'
 
 import styles from './AllAudiences.module.scss'
 import buttonStyles from 'components/parts/Button/ButtonThemes.module.scss'
-import tableStyles from 'components/Table/TableBase.module.scss'
+import tableStyles from 'components/TableDiv/TableBase.module.scss'
+import checkMenuStyles from 'components/TableDiv/CheckMenu/CheckMenu.module.scss'
 import ddStyles from 'components/parts/DropDown/DropDown.module.scss'
 
 import { IPageData } from 'types'
 
-import { IconCheck } from 'assets/icons'
+import { IconCheck, IconCopy, IconTrash } from 'assets/icons'
 import { data } from './data'
+import CheckMenu from 'components/TableDiv/CheckMenu/CheckMenu'
 
 const header = ['', 'ID', 'Название', 'Количество контактов', 'Дата создания', 'Дата изменения']
 
@@ -28,8 +30,10 @@ const AllAudiences: FC<IPageData> = () => {
   }
 
   const [checkedList, setCheckedList] = useState<number[]>([])
-  const [lastTriggeredCheck, setLastTriggeredCheck] = useState<number>(0)
   const simplebarRef = useRef<SimpleBar>(null)
+
+  const totalCountOFData = data.length ///TODO когда будет бк нужно будет переделать
+  const checkedCount = checkedList.length
 
   const isItChecked = (id: number) => {
     return checkedList.includes(id)
@@ -37,38 +41,13 @@ const AllAudiences: FC<IPageData> = () => {
 
   const checkMenuIsOpen = () => checkedList.length > 0
 
-  const toggleCheck = (e: any) => {
+  const toggleCheck = (e: React.MouseEvent<HTMLElement>) => {
     const { id } = e.currentTarget.dataset
     const idNum = Number(id)
     if (isItChecked(idNum)) {
       const newChecked = checkedList.filter((el) => el !== idNum)
       setCheckedList(newChecked)
     } else setCheckedList([...checkedList, idNum])
-    setLastTriggeredCheck(idNum)
-    const idOfLastElement = data[data.length - 1].id
-    if (idNum === idOfLastElement) e.currentTarget.scrollIntoView()
-  }
-
-  useEffect(() => {
-    const idOfLastElement = data[data.length - 1].id
-    console.log(lastTriggeredCheck, idOfLastElement)
-    if (lastTriggeredCheck === idOfLastElement) scrollToBottom()
-    // if (!checkMenuIsOpen()) scrollToBottom()
-  }, [lastTriggeredCheck, checkMenuIsOpen])
-
-  const recalculate = () => {
-    const scrollNode = simplebarRef.current
-    console.log('recalculate!')
-    if (scrollNode) scrollNode.recalculate()
-  }
-
-  const scrollToBottom = () => {
-    console.log('scrollToBottom!')
-    const scrollNode = simplebarRef.current
-    if (!scrollNode) return
-    const scrollElement = scrollNode.getScrollElement()
-    const scrollHight = scrollElement.scrollHeight
-    // scrollElement.scrollTo({ top: scrollHight, behavior: 'smooth' })
   }
 
   return (
@@ -97,26 +76,36 @@ const AllAudiences: FC<IPageData> = () => {
           </div>
         </DropDown>
       </PageHead>
-      {/* <button onClick={recalculate}>recalc</button> */}
       <ScrollTable
         headers={header}
         handleScrollLimit={() => console.log('handleScrollLimit')}
         simplebarRef={simplebarRef}
         checkMenu={
           checkMenuIsOpen() && (
-            <div className={cx(tableStyles.checkMenu)}>
-              {/* <button onClick={recalculate}>recalc</button> */}
-            </div>
+            <CheckMenu checkedCount={checkedCount} total={totalCountOFData}>
+              <button className={cx(checkMenuStyles.button, 'text_1')}>
+                <IconCopy className={checkMenuStyles.buttonIcon} />
+                Копировать
+              </button>
+              <button className={cx(checkMenuStyles.button, 'text_1')}>
+                <IconTrash className={checkMenuStyles.buttonIcon} />
+                Удалить
+              </button>
+            </CheckMenu>
           )
         }
       >
         {data.map((dataRow, index) => {
-          const { id, name, contact_count, create_date, last_update_date } = dataRow
+          const { name, contact_count, create_date, last_update_date } = dataRow
           const checked = isItChecked(index)
           return (
             <div className={tableStyles.row} key={index} onClick={toggleCheck} data-id={index}>
-              <div className="checkContainer">
-                <div className={cx(tableStyles.check, { [tableStyles.checked]: checked })}>
+              <div className={cx(tableStyles.cell, 'checkContainer')}>
+                <div
+                  className={cx(tableStyles.check, {
+                    [tableStyles.checked]: checked,
+                  })}
+                >
                   {checked && <IconCheck />}
                 </div>
               </div>
@@ -132,14 +121,16 @@ const AllAudiences: FC<IPageData> = () => {
             </div>
           )
         })}
-        {checkMenuIsOpen() && (
-          <div className={tableStyles.row}>
-            <div className={tableStyles.cell}></div>
-          </div>
-        )}
+        {true && <EmptyRow />}
       </ScrollTable>
     </div>
   )
 }
+
+const EmptyRow = () => (
+  <div className={tableStyles.row}>
+    <div className={tableStyles.cell}></div>
+  </div>
+)
 
 export default AllAudiences
