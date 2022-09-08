998e822acc5dc3835720faab44d264939d10b93d
Egor Solonin <e.solonin@onpoint.ru>
2022-09-08 03:34:07 +0300

    сохраняю токен в cookie

--- src/store/auth/action-type.ts
+++ src/store/auth/action-type.ts
@@ -1,6 +1,5 @@
 const ActionType = {
   SET_USER: `SET_USER`,
-  SET_AUTH_DATA: `SET_AUTH_DATA`,
 }
 
 export default ActionType
--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -6,10 +6,6 @@ const ActionCreator = {
     type: ActionType.SET_USER,
     payload: { user },
   }),
-  setAuthData: (authData: IStoreAuth[StoreKeys.authData]) => ({
-    type: ActionType.SET_AUTH_DATA,
-    payload: authData,
-  }),
 }
 
 export default ActionCreator
